162778ebe1db3b44599a642e3c82ef56c2f94595
MailaFAP <maila_3@hotmail.com>
2023-04-30 19:25:24 -0300

    função isvalid e maskify

--- src/index.js
+++ src/index.js
@@ -2,29 +2,20 @@ import validator from './validator.js';
 
 console.log(validator);
 
-//declarei a variável número de cartão preenchida no HTML com id= numeroCartao
 
-function validar(){
-    //fazer uma funçao que pegue o número do cartão digitado e monte um array
-    const cardNumber = (document.getElementById("numeroCartao").value);
+//assim que clicar em confirmar a função tem que rodar.Então, peguei o botão e coloquei um addEventListener para isso acontecer.
+document.getElementById("button").addEventListener("click", validar);
+function validar() {
+    //fazer uma funçao que pegue o número do cartão digitado e monte um array    
     //separar o número do cartão em elementos individuais (pois ele ainda será um número inteiro se colocar somente o parseInt)
-    const newNumber = cardNumber.map(function(element){
-        return parseInt(element);
-    })
-    newNumber.reverse()
-    //pegar o número de duas em duas casas e multiplicar por dois
-    const otherArray = [];
-    for (let i = 0; i < newNumber.length; i=i+2) {
-        otherArray.push(newNumber[i]);
-      }
-
-    console.log(otherArray)
-    
+    if(validator.isValid(document.getElementById("numberCard").value)) {
+        alert("Cartão válido");
+    } else {
+        alert("Cartão inválido");
+    }
 }
 
-
-//somar os digitos de números decimais 
-//somar os valores resultantes nos vetores do array 
-
-//se o valor final for do número decimal por zero, o número do cartão é válido
-//se não, número de cartão inválido
+document.getElementById("numberCard").addEventListener("keydown", mascarar);
+function mascarar() {
+    document.getElementById("numberCard").value = validator.maskify(document.getElementById("numberCard").value); 
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,52 @@
 const validator = {
   // ...
+  isValid: function (numberCard) {
+    const newNumber = numberCard.map(function (element) {
+      return parseInt(element);
+    });
+    newNumber.reverse();
+    console.log(newNumber);
+    //***pegar o número de duas em duas casas e multiplicar por dois
+    //montar um novo array  em branco do mesmo tamanho do array anterior
+    let otherArray = new Array(newNumber.length);
+    //**declarar uma variável com valor zero para soma de todos os valores antes do for  */
+    let sum = 0;
+    //percorrer o array elemento por elemento : declara o index=0 , dá consição de se i é maior que o tamanho da array newNumber, e mostra qual o incremento, que será adicionar a posição o valor do index
+    for (let i = 0; i < newNumber.length; i++) {
+      //se o resto da divisão de i por 2 for 0, multiplicar o valor por 2    
+      if (i % 2 === 0) {
+        otherArray[i] = newNumber[i] * 2;
+        //se o valor de i for maior que 9, somar os digitos
+        if (otherArray[i] > 9) {
+          //declarar uma nova array para obter os digitos como string e separa-las
+          const addDigits = otherArray[i].toString().split('');
+          //passar o valor obtido como string em número separados e conseguir somá-los
+          otherArray[i] = parseInt(addDigits[0]) + parseInt(addDigits[1]);
+        }
+      } else {
+        //se o valor do resto da divisão de i por 2 não for zero, somente copiar o valor de i
+        otherArray[i] = newNumber[i];
+      }
+      //somar os elementos e atribuir á variavel soma 
+      sum = sum + otherArray[i];
+    } 
+    if (sum % 10 === 0) {
+      
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+
+  maskify: function (numberCard) {   
+    //como fazer a função dar certo com menos de 4 digitos  
+    let str = numberCard.substring(0, numberCard.length - 4);
+    let num = "#".repeat(str.length);
+    let end = numberCard.substring(numberCard.length - 4);
+    return num.concat(end);
+  }
+
 };
 
 export default validator;
