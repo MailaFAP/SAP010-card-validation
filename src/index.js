62176e3fe90001d1616d331e32434b74f855910d
MailaFAP <maila_3@hotmail.com>
2023-05-04 22:28:59 -0300

    termino das funções isValid e Maskify com testes 100%

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import validator from './validator.js';
 
-console.log(validator);
 
-
-//assim que clicar em confirmar a função tem que rodar.Então, peguei o botão e coloquei um addEventListener para isso acontecer.
-document.getElementById("button").addEventListener("click", validar);
-function validar() {
-    //fazer uma funçao que pegue o número do cartão digitado e monte um array    
-    //separar o número do cartão em elementos individuais (pois ele ainda será um número inteiro se colocar somente o parseInt)
-    if(validator.isValid(document.getElementById("numberCard").value)) {
-        alert("Cartão válido");
-    } else {
-        alert("Cartão inválido");
-    }
+document.getElementById("button").addEventListener("click", validar);//assim que clicar em confirmar a função tem que rodar.Então, peguei o botão e coloquei um addEventListener para isso acontecer.
+function validar(event) {
+  event.preventDefault();//fazer uma funçao que pegue o número do cartão digitado e monte um array    
+  if (validator.isValid(document.getElementById("hidden").value)) {//separar o número do cartão em elementos individuais (pois ele ainda será um número inteiro se colocar somente o parseInt)
+    alert("Cartão válido");
+  } else {
+    alert("Cartão inválido");
+  }
 }
 
-document.getElementById("numberCard").addEventListener("keydown", mascarar);
-function mascarar() {
-    document.getElementById("numberCard").value = validator.maskify(document.getElementById("numberCard").value); 
+document.getElementById("numberCard").addEventListener("keyup", mascarar);
+function mascarar(e) {
+  const letra = e.key;
+  document.getElementById("hidden").value = document.getElementById("hidden").value + letra;
+  document.getElementById("numberCard").value = validator.maskify(document.getElementById("numberCard").value);
 }
--- src/validator.js
+++ src/validator.js
@@ -1,20 +1,21 @@
 const validator = {
   // ...
-  isValid: function (numberCard) {
-    const newNumber = numberCard.map(function (element) {
+  isValid: function (numero) {
+    //pegar o parametro e transformar em um array passando de string para número
+    const newNumber = numero.split("").map(function (element) {
       return parseInt(element);
     });
+    //inverter o array
     newNumber.reverse();
-    console.log(newNumber);
-    //***pegar o número de duas em duas casas e multiplicar por dois
+    //pegar o número de duas em duas casas e multiplicar por dois
     //montar um novo array  em branco do mesmo tamanho do array anterior
-    let otherArray = new Array(newNumber.length);
-    //**declarar uma variável com valor zero para soma de todos os valores antes do for  */
+    const otherArray = new Array(newNumber.length);
+    //declarar uma variável com valor zero para soma de todos os valores antes do for 
     let sum = 0;
-    //percorrer o array elemento por elemento : declara o index=0 , dá consição de se i é maior que o tamanho da array newNumber, e mostra qual o incremento, que será adicionar a posição o valor do index
+    //percorrer o array elemento por elemento : declara o index=0 , dá condição de se i é menor que o tamanho da array newNumber, e mostra qual o incremento, que será adicionar a posição o valor do index
     for (let i = 0; i < newNumber.length; i++) {
       //se o resto da divisão de i por 2 for 0, multiplicar o valor por 2    
-      if (i % 2 === 0) {
+      if (i % 2 !== 0) {
         otherArray[i] = newNumber[i] * 2;
         //se o valor de i for maior que 9, somar os digitos
         if (otherArray[i] > 9) {
@@ -29,24 +30,28 @@ const validator = {
       }
       //somar os elementos e atribuir á variavel soma 
       sum = sum + otherArray[i];
-    } 
+    }
     if (sum % 10 === 0) {
-      
+      //se o resultado da soma %10 for ===0, retornar verdadeiro para a função isValid, se não, retornar falso
       return true;
     } else {
       return false;
     }
   },
 
-
-  maskify: function (numberCard) {   
-    //como fazer a função dar certo com menos de 4 digitos  
-    let str = numberCard.substring(0, numberCard.length - 4);
-    let num = "#".repeat(str.length);
-    let end = numberCard.substring(numberCard.length - 4);
-    return num.concat(end);
+  //mascarar os números obtidos no input numberCard
+  maskify: function (numberCard) {
+    const maskNumber = [];
+    for (let i = 0; i < numberCard.length; i++) {
+      if (i < numberCard.length - 4) {
+        maskNumber.push("#");
+      } else {
+        maskNumber.push(numberCard[i]);
+      }
+    }
+    const maskEnd = maskNumber.join("");
+    return maskEnd;
   }
-
 };
 
 export default validator;
